35a63e0fc4b4d15d2a58e84fd60feb7a49c87487
Philip Brohan <philip@Philips-MacBook-Pro.local>
2018-10-29 10:57:15 +0000

    Typo fix.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='Ofline Assimilation',
+    name='Offline Assimilation',
     version='0.0.1',
     description='Improving reanalysis with offline assimilation',
 
